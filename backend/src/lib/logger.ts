864fa46a7e0ac0eb8a80c98b99879e74b7adedaf
Nafish Ahmed <nafish.ahmed.dev@gmail.com>
2023-06-24 18:47:55 +0530

    iinit

--- backend/src/lib/logger.ts
+++ backend/src/lib/logger.ts
@@ -1,10 +1,10 @@
 import chalk from "chalk";
 import moment from "moment";
 const consola = {...console};
-console.log = (...args) => consola.log(`[${moment().format("YYYY-MM-DD HH:MM")}]`,chalk.green("[ LOG ]"),...args);
-console.info = (...args) => consola.info(`[${moment().format("YYYY-MM-DD HH:MM")}]`,chalk.blue("[INFO ]"),...args);
-console.warn = (...args) => consola.warn(`[${moment().format("YYYY-MM-DD HH:MM")}]`,chalk.yellow("[WARN ]"),...args);
-console.error = (...args) => consola.error(`[${moment().format("YYYY-MM-DD HH:MM")}]`,chalk.red("[ERROR]"),...args);
-console.debug = (...args) => consola.debug(`[${moment().format("YYYY-MM-DD HH:MM")}]`,chalk.yellow("[DEBUG]"), ...args);
+console.log = (...args) => consola.log(`[${moment().format("YYYY-MM-DD HH:MM:ss")}]`,chalk.green("[ LOG ]"),...args);
+console.info = (...args) => consola.info(`[${moment().format("YYYY-MM-DD HH:MM:ss")}]`,chalk.blue("[INFO ]"),...args);
+console.warn = (...args) => consola.warn(`[${moment().format("YYYY-MM-DD HH:MM:ss")}]`,chalk.yellow("[WARN ]"),...args);
+console.error = (...args) => consola.error(`[${moment().format("YYYY-MM-DD HH:MM:ss")}]`,chalk.red("[ERROR]"),...args);
+console.debug = (...args) => consola.debug(`[${moment().format("YYYY-MM-DD HH:MM:ss")}]`,chalk.yellow("[DEBUG]"), ...args);
 
 export default {};
--- backend/src/middleware/RequestLogger.ts
+++ backend/src/middleware/RequestLogger.ts
@@ -14,21 +14,21 @@ export default function RequestLogger(req: any, res:any, next : any){
     };
     console.log("\n\n")
     const startTime = moment();
-    console.log(`-----------REQUEST START`, chalk.bgRed(" ID:", req.id,), "-----------");
+    console.log(`----------- REQUEST START` ,`[`, chalk.red(req.id), `]`, "-----------");
     console.log(chalk.bgGreenBright(chalk.bgYellowBright(req.method), req.originalUrl));
-    console.log(chalk.blue("HEADER"), ": ",req.headers);
-    console.log(chalk.blue("PARAMS"), ": ",req.params);
-    console.log(chalk.blue("QUERY "), ": ",req.query);
-    console.log(chalk.blue("BODY  "), ": ",req.body);
+    console.log(chalk.blue("Headers"), "→",req.headers);
+    console.log(chalk.blue("Params "), "→",req.params);
+    console.log(chalk.blue("Query  "), "→",req.query);
+    console.log(chalk.blue("Body   "), "→",req.body);
     res.send = resDotSendInterceptor(res, res.send);
     res.on("finish", () => {
-        console.log(chalk.blue(`RESPONSE`), `${moment.duration(moment().diff(req.time)).asMilliseconds()}ms`);
+        console.log(chalk.blue(`Response`), `${moment.duration(moment().diff(req.time)).asMilliseconds()}ms`);
         try{
             console.log(JSON.stringify(JSON.parse(res.contentBody), null, 2))
         } catch (e){
             console.log(res.contentBody);
         }
-        console.log(`-----------REQUEST END IN`, moment().diff(startTime)+"ms", chalk.bgRed(" ID:", req.id,), "-----------");
+        console.log(`----------- REQUEST END` ,`[`, chalk.red(req.id), "]", "-----------");
     });
     next();
 };
