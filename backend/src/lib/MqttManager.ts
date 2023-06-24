479d27732e03d063122c4ee5d8ac5d21024831e3
Nafish Ahmed <nafish.ahmed.dev@gmail.com>
2023-06-25 03:07:45 +0530

    iinit

--- backend/src/lib/MqttManager.ts
+++ backend/src/lib/MqttManager.ts
@@ -2,15 +2,11 @@ import mqtt from "mqtt";
 
 export default async function MqttManager() {
     const client = mqtt.connect(process.env.MQTT_ENDPOINT as string);
-    await new Promise((resolve, reject)=>client.on('connect', function (err) {
-        if(!client.connected) reject(new Error("Unable to connect mqtt"));
-        resolve(true);
-        console.log("MQTT Connected");
+    client.on('connect', function (err) {
         client.subscribe('message');
-        // client.publish('message', "hello");
-    }));
+    });
 
-    client.on('message', function (topic : string, message : any) {
+    client.on('message', function (topic: string, message: any) {
         console.log(topic, message.toString())
     });
 
