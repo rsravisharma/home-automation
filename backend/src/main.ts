effb4b6b9ec8190961a7b30a7c06acb292cf4ed6
Nafish Ahmed <nafish.ahmed.dev@gmail.com>
2023-06-25 02:36:37 +0530

    iinit

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,8 @@ import http from "http";
 import dotenv from "dotenv";
 import bodyParser from "body-parser";
 import cors from "cors";
+import { Server } from "socket.io";
+const { instrument } = require("@socket.io/admin-ui");
 dotenv.config();
 import "./lib/logger";
 import * as models from "./models";
@@ -31,12 +33,26 @@ const boot = async () => {
     app.use(RequestLogger);
     app.set("mqtt", mqttClient);
     await router(app);
+    const server = http.createServer(app);
+
+    console.log("\n\n-----------------------------------------------------------------------")
+    console.log("Launching socket.io");
+    console.log("");
+    const io = new Server(server, {
+        cors: {
+            origin: "*"
+        }
+    });
+    instrument(io, {
+        auth: false,
+        mode: "development",
+      });
+    app.set("io", io);
 
 
     console.log("\n\n-----------------------------------------------------------------------")
     console.log("Launching server");
     const port = Number(process.env.PORT || 3001);
-    const server = http.createServer(app);
     server.listen(port, () => {
         console.log("HTTP Server is listening to port : ", port);
     })
--- backend/src/routes/index.router.ts
+++ backend/src/routes/index.router.ts
@@ -2,5 +2,14 @@ import { Router } from "express";
 
 const router = Router({mergeParams: true});
 
+router.get("/", (req : any, res : any)=>{
+    const io = req.app.get("io");
+    io.emit("message", "Hello I'm running....");
+    console.log(io);
+    res.json({
+        resultCode: "SUCCESS",
+        message: "Application is runniing...."
+    })
+})
 
 export default router;
