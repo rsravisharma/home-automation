376b86cb4bfbbcb273e25d425df606078651bded
Ravi Sharma <rsravisharma38@gmail.com>
2023-06-25 18:37:20 +0530

    frontned Deleted

--- frontend/src/__mocks__/account.jsx
+++
@@ -1,5 +0,0 @@
-export default {
-    "user_name": "guest",
-    "email": "guest@xxx.com",
-    "phone_number": "12345678",
-};
--- frontend/src/pages/Maps.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import AllMessagesMap from "../components/shared/AllMessagesMap";
-import { Navigate } from 'react-router-dom';
-
-const Maps = ({height}) => {
-    return <AllMessagesMap height={height}/>
-};
-
-export default Maps;
